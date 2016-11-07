5ce340c81487e3046de914bced916efed049098c
Alberto Sierra <alberto.sierra@murciaeduca.es>
2016-11-07 09:44:15 +0100

    Autoload de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,5 +1,16 @@
 var models = require('../models/models.js');
 
+// Autoload - factoriza el codigo se ruta incluye :quizId
+exports.load = function(req, res, next, quizId) {
+	models.Quiz.findById(quizId).then(
+		function(quiz){
+			if (quiz) {
+				req.quiz = quiz;
+				next();
+			}	
+		})
+};
+
 // GET /quizes/:quizId(\\d+)
 exports.index = function(req, res) {
 	models.Quiz.findAll().then(function(quizes){
@@ -10,18 +21,14 @@ exports.index = function(req, res) {
 
 // GET /quizes/:quizId(\\d+)
 exports.show = function(req, res) {
-	models.Quiz.findById(req.params.quizId).then(function(quiz){
-		res.render('quizes/show', {quiz: quiz});
-	})
+	res.render('quizes/show', {quiz: req.quiz});
 };
 
 // GET /quizes/:quizId(\\d+)/answer
 exports.answer = function(req, res) {
-	models.Quiz.findById(req.params.quizId).then(function(quiz){
-		if (req.query.respuesta === quiz.respuesta) {
-			res.render('quizes/answer', {quiz: quiz, respuesta: 'Correcto'});
-		} else {
-			res.render('quizes/answer', {quiz: quiz, respuesta: 'Incorrecto'});
-		}
-	})
+	if (req.query.respuesta === req.quiz.respuesta) {
+		res.render('quizes/answer', {quiz: req.quiz, respuesta: 'Correcto'});
+	} else {
+		res.render('quizes/answer', {quiz: req.quiz, respuesta: 'Incorrecto'});
+	}
 };
